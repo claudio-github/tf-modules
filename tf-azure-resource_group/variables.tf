45158d1100023f0970e6f55c9917b4d4b0490a2e
Claudio Toledo <claudiotoledoit@gmail.com>
2022-10-03 16:37:22 -0300

    First commit

---
+++ tf-azure-resource_group/variables.tf
@@ -0,0 +1,10 @@
+variable "name" {
+  description = "(Required) Resource Group name"
+  type = string 
+}
+
+variable "location" {
+  description = "(Required) Resource Group location"
+  type = string
+}
+
