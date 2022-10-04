8acb25a37075ed1459160eca91b885a9ed29adb0
Claudio Toledo <claudiotoledoit@gmail.com>
2022-10-03 21:56:41 -0300

    Created Service Plan module

--- tf-azure-service_plan/main.tf
+++ tf-azure-service_plan/main.tf
@@ -2,6 +2,6 @@ resource "azurerm_service_plan" "example" {
   name                = var.name
   resource_group_name = var.resource_group_name
   location            = var.location
-  os_type             = "Linux"
-  sku_name            = "P1v2"
+  os_type             = var.os_type
+  sku_name            = var.sku_name
 }
--- tf-azure-service_plan/variables.tf
+++ tf-azure-service_plan/variables.tf
@@ -12,3 +12,13 @@ variable "location" {
   description = "(Required) Service Plan location"
   type = string
 }
+
+variable "os_type" {
+  description = "(Required) SO used by service plan"
+  type = string
+}
+
+variable "sku_name" {
+  description = "(Required) SKU used "
+  type = string
+}
